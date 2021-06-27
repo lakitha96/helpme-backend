de556b81d8a3bd3e3632bcc52b34040d02b4885e
lakitha <lakithaprabudh@arimaclanka.com>
2021-06-27 16:30:54 +0530

    initial commit

---
+++ src/main/java/com/bedfordshire/helpmebackend/HelpmeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bedfordshire.helpmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpmeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpmeBackendApplication.class, args);
+	}
+
+}
